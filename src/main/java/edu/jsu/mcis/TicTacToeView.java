40571aa17f1895671d3ae21c5d69a9ee6911e900
BrianCWillis <briancwillis2010@yahoo.com>
2017-09-20 11:33:09 -0500

    All tests passed! Onto final methods.

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -3,21 +3,30 @@ package edu.jsu.mcis;
 public class TicTacToeView {
 
     private TicTacToeModel model;
-    
-    /* CONSTRUCTOR */
 	
-    public TicTacToeView(TicTacToeModel model) {
-        
+    public TicTacToeView(TicTacToeModel model){
         this.model = model;
-        
     }
 	
-    public void viewModel() {
+    public void viewModel(){
         
         /* Print the board to the console (see examples) */
         
-        /* INSERT YOUR CODE HERE */
-
+        String output = "\n  012\n";
+		
+		String line = "";
+		for(int i=0; i<model.getWidth(); i++){
+			line = "\n" + Integer.toString(i) + " ";
+			for(int j=0; j<model.getWidth(); j++){
+				if(model.getMark(i, j) == TicTacToeModel.Mark.EMPTY)
+					line += "-";
+				else
+					line += model.getMark(i, j).toString();
+			}
+			output += line;
+			line = "";
+		}
+		System.out.print(output + "\n\n\n\n");
     }
 
     public void showNextMovePrompt() {
